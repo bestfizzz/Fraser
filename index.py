449523cfe6ab7a235969f4b57dfcedcfa35f5345
bestfizzz <52324074+bestfizzz@users.noreply.github.com>
2022-08-20 11:56:31 +0700

    40%

--- index.py
+++ index.py
@@ -5,19 +5,24 @@ hotel = [['A', 'A', 'A', 'A'],
 def logRoom(roomlist, roomType):
     a = 'available' if roomType=='A' else 'occupied' if roomType=='O' else 'reserved'
     print('The {} rooms are: '.format(a),', '.join(roomlist))
-
-def showRooms(list,roomType):
+def showRooms(roomType):
     valid=['A','R','O']
     roomList=[]
     roomType=roomType.upper()
     if roomType not in valid:
         return print('invalid input')
         
-    for i in range(0,len(list)):
-        for j in range(0,len(list[i])):
-            if roomType==list[i][j]:
+    for i in range(0,len(hotel)):
+        for j in range(0,len(hotel[i])):
+            if roomType==hotel[i][j]:
                 roomList.append('{}.{}'.format(i+1,j+1))
             j=j+1
         i=+1
     return logRoom(roomList,roomType)
-showRooms(hotel,'R')
+showRooms('R')
+def changeState(roomNumber,stateChange):
+    floor=int(roomNumber[:roomNumber.index(".")].strip())
+    room=int(roomNumber[roomNumber.index(".")+1:].strip())
+    hotel[floor-1][room-1]=stateChange.upper()
+    print(hotel)
+changeState('3.4','o')
